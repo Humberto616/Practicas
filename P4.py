1cbc2a0771e45eaa9f93daeb6787662657074401
Humberto616 <a19311008@uthermosillo.edu.mx>
2022-05-16 18:02:27 -0700

    Se ralizo la practica 4

---
+++ P4.py
@@ -0,0 +1,13 @@
+#Practica 4
+#Area de un traingulo
+#Cervantes Humberto
+B=32
+A=47
+R=(B*A)/2
+print("El area del triangulo es: ", R, str("m2"))
+
+
+#Declaración de variables
+#B=Base
+#A=Altura
+#R=Resultado
